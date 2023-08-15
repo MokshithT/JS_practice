64488768375ac64277e0d815e734cb0526a2576a
Mokshith Thota <109408612+thotamokshith@users.noreply.github.com>
2023-08-15 14:25:15 +0530

    Add files via upload
    Constants in javascript: briefly explained with examples

---
+++ 1.2 Constants.js
@@ -0,0 +1,18 @@
+/*
+constants
+implemented by keyword: const
+*/
+
+//Without const : the variables value can be changed.
+let intrestRate = 0.3;
+intrestRate = 1;
+console.log(intrestRate);
+
+//with const 
+const intrestRate1 = 0.3;
+console.log(intrestRate1);
+
+/**
+ the value cannot be change using const keyword
+ */
+
