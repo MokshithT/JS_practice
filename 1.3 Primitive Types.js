4cbf9d30667f2dd585db08ed29b2e35d0391a497
Mokshith Thota <109408612+thotamokshith@users.noreply.github.com>
2023-08-15 14:44:25 +0530

    Add files via upload
    Primitive Types in js

---
+++ 1.3 Primitive Types.js
@@ -0,0 +1,24 @@
+//Primitive types:
+/**
+ * String
+ * Number
+ * Boolean
+ * undefined
+ * null
+ */
+
+//String Literal
+let name = "Hello 6";
+
+//Number Literal
+let Number = 6;
+
+//Boolean Literal
+let isApproved = true;
+let Approved = false;
+
+//undefined
+let firstName;
+
+//null
+let selectedColor = null;
