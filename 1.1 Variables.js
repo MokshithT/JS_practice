4a9a156dacfd8f5bc23dc023e47d8f9f66a04d04
Mokshith Thota <109408612+thotamokshith@users.noreply.github.com>
2023-08-15 14:15:44 +0530

    Add files via upload
    Variables: how to declare variables and described about camel Notations.

---
+++ 1.1 Variables.js
@@ -0,0 +1,27 @@
+
+// variables 1.1 
+/* 
+varaiables cannot be reserved keywords
+should be meaningful & descriptive 
+shouldnot start with a number
+cannot contain a space or hyphen(-)
+*/
+
+let name = "Subramanya Swami";
+
+console.log(name);
+
+//camel Notations
+/*
+-> if we have multiple word variable names we have to put them together 
+the first letter of the first word should start with lower case and next words should 
+be uppercsae 
+ex: firstName
+lastName
+rollNo
+ */
+
+
+
+
+
