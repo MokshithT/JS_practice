61e0cadc97913d8b9dceffe75ab990e87e7c42d7
Mokshith Thota <109408612+thotamokshith@users.noreply.github.com>
2023-08-16 18:11:00 +0530

    Add files via upload

---
+++ 1.4 Reference Types.js
@@ -0,0 +1,33 @@
+//Reference Types
+/**
+ * Object
+ * Array
+ * Function
+ */
+
+//objects:
+/**
+ * Think of a person
+ * A person has name, age , address and so on..
+ * these are the properties of a person.
+ */
+
+/*when we are dealing with multiple related variables 
+we can put that variables inside an object.
+*/
+
+//ex: a person object
+
+let person = {
+    name : "Moksh",
+    rollNo : 755
+};
+
+console.log(person)
+
+//values are stored in key : value pairs. : keys are properties.
+
+// { } <- object literal
+
+//when we want to change the values of properties the objects
+
